02e3cc5d18ee6e40644231fa5a03dc43a69bec6f
renandacruzzsnts <cleomardestak@gmail.com>
2025-05-28 20:08:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+def soma(a, b
+    return a + b
+
+def saudacao(nome)
+    print("Olá," + nome)
+
+def dividir(a, b):
+    if b == 0:
+        resultado = a / b
+    return resultado
+
+idade = "20"
+if idade > 18:
+    print("Maior de idade")
+
+print("Isso é um log que não deveria estar em produção")
+
+def nao_usada(x):
+    return x * 2
+
+lista = [1, 2, 3]
+for i in range(5)
+    print(lista[i])
